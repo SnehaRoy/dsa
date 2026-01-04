b348228f6007cde3bc224bf96b8fac3786bba849
Sneha Roy <rsneha804@gmail.com>
2026-01-04 19:57:39 +0530

    longest common prefix

---
+++ neetcode/longest_common_prefix.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+
+def longest_common_prefix(lists):
+    prefix = lists[0]
+
+    for i in range(1,len(lists)):
+        while(not lists[i].startswith(prefix)):
+            prefix = prefix[0:len(prefix) - 1]
+
+    return prefix
+
+print(longest_common_prefix(['flower', 'flow', 'flight']))
